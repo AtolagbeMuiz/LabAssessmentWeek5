4a689a7fdf0f5e6d70e190d25951e7861efa0124
Atolagbe Muiz <atolagbemuiz@gmail.com>
2021-05-16 20:50:26 +0100

    latest commit

--- BankA.cs
+++ BankA.cs
@@ -3,6 +3,6 @@ public class BankA : Bank
     private string balance = "$100";
     public override void getBalance()
     {
-        System.Console.WriteLine("Your balance in bankA is " + balance);
+        System.Console.WriteLine("Your balance in BankA is " + balance);
     }
 }
